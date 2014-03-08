80fcbbd1daba7ddac2903c253884182772dc5d45
Michael Landis <mlandis@gmail.com>
2014-03-07 22:52:27 -0800

    Quick fix, though still css problems re: text

--- js/bayfig.js
+++ js/bayfig.js
@@ -83,7 +83,7 @@ Bayfig.redrawDivs = function() {
     this.divDrawOffset = $('#divDraw').offset();
     console.log(this.canvaswidth,this.mapwidth,this.divDrawOffset.left);
     //$( '#divText' ).css({left:this.canvaswidth+this.mapwidth+this.divDrawOffset.left+10,top:this.mapheight+this.divDrawMargin});
-    $( '#divText' ).css({left:this.canvaswidth+this.divDrawOffset.left+30,top:this.mapheight+this.divDrawMargin});
+    $( '#divText' ).css({left:this.canvaswidth+this.divDrawOffset.left+this.mapwidth+10,top:this.mapheight+this.divDrawMargin});
 };
 
 Bayfig.parseInput = function() {
